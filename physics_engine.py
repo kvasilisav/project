7547029ac2eac57ba88ab72e1f72ed3d6ec49555
ProtasevichTanya <protasevich.ts@phystech.edu>
2021-12-17 14:38:29 +0300

    final physics

--- physics_engine.py
+++ physics_engine.py
@@ -18,13 +18,14 @@ def init(screen):
     height -= ground
 
 class Vector:
-    """ что-то надо написать """
+    """класс векторов"""
     def __init__(self, magnitude = 0, angle = radians(0)):
         self.magnitude = magnitude
         self.angle = angle
 
 def add_vectors(vector1, vector2):
-    '''Складываем векторы'''
+    '''функция складывает векторы (например скорости)
+        vector1, vector2 - элементы класса вектор  '''
     x = sin(vector1.angle)*vector1.magnitude + sin(vector2.angle)*vector2.magnitude
     y = cos(vector1.angle)*vector1.magnitude + cos(vector2.angle)*vector2.magnitude
     '''Находим угол и величину получившегося вектора'''
@@ -38,15 +39,15 @@ gravity = Vector(0.2, pi)
 friction = 0.99
 elasticity = 0.8
 block_elasticity = 0.7
-h = 1
 
 class Pig:
+    '''класс свинок'''
     def __init__(self, x, y, r, v = None, type = "PIG", loaded = False, color = (255, 255, 255)):
+        '''координаты и параметры свинок'''
         self.x = x
         self.y = y
         self.r = r
-        self.h = h
-        self.shot = 2 ###
+        self.shot = 2 
         if v == None:
             self.velocity = Vector()
         else:
@@ -73,6 +74,8 @@ class Pig:
         self.isDead = False 
 
     def draw(self):
+        '''функция рисует свинью или птицу в зависимости от типа объекта,
+        также отвечает за прорисовку пути птицы'''
         self.animate_count += 1
 
         if (self.type == "BIRD") and (not self.loaded):
@@ -82,17 +85,18 @@ class Pig:
         if (self.type == "PIG") and (not self.animate_count%20) and (not self.isDead):
             self.image = random.choice([self.pig1_image, self.pig2_image])
 
-        display.blit(self.image, (self.x - self.r, self.y - self.r))###
+        display.blit(self.image, (self.x - self.r, self.y - self.r))
 
 
     def dead(self):
-        '''контроль смерти'''
+        '''функция, которая проверяет значение логической переменной isDead
+        и в случае смерти птицы заменяет её на соответствующее изображение'''
         self.isDead = True
         self.image = self.pig_dead
 
     def move(self):
-        '''движение'''
-        self.velocity = add_vectors(self.velocity, gravity) #складываем скорость с ускорением свободного падения
+        '''движение с учетом ускорения свободного падения'''
+        self.velocity = add_vectors(self.velocity, gravity) 
 
         self.x += self.velocity.magnitude*sin(self.velocity.angle)
         self.y -= self.velocity.magnitude*cos(self.velocity.angle)
@@ -100,7 +104,7 @@ class Pig:
         self.velocity.magnitude *= friction
 
         if self.x > width - 2 * self.r:
-            self.x = 2*(width - self.r * 2) - self.x
+            self.x = 2*(width - 2 * self.r) - self.x
             self.velocity.angle *= -1
             self.velocity.magnitude *= elasticity
         elif self.x < self.r:
@@ -122,26 +126,26 @@ class Pig:
             self.path.append((self.x, self.y))
 
 class Bird(Pig):
+    '''класс птицы (наследует у класса свиньи)'''
     def load(self, slingshot):
-        '''птица на рогатке'''
+        '''инициализация птицы на рогатке'''
         self.x = slingshot.x
         self.y = slingshot.y
         self.loaded = True
 
     def mouse_selected(self):
-        '''изменить выстрел!!!!!!!!!!!!!!!!!!!'''
+        '''функция отвечает за прицеливание'''
         pos = pygame.mouse.get_pos()
         dx = pos[0] - self.x
         dy = pos[1] - self.y
         dist = (dy**2 + dx**2)**0.5
-        if dist < 2 * self.r: #это условие того, что мышка наведена на птицу
+        if dist < 2 * self.r:
             return True
-        
         
         return False 
 
     def reposition(self, slingshot, mouse_click):
-        """ функция отвечает за перемещение? """
+        '''расчет начальной скорости в зависимости от растяжения рогатки'''
         pos = pygame.mouse.get_pos()
         if self.mouse_selected():
             self.x = pos[0]
@@ -150,31 +154,33 @@ class Bird(Pig):
             dx = slingshot.x - self.x
             dy = slingshot.y - self.y
             
-            self.velocity.magnitude = int(hypot(dx, dy)/8) #задаем величину скорости
+            self.velocity.magnitude = int(hypot(dx, dy)/8) 
             if self.velocity.magnitude > 80:
                 self.velocity.magnitude = 80
             self.velocity.angle = pi/2 + atan2(dy, dx)
 
     def unload(self):
-        """ функция, которая проверяет значение логической переменной loaded (присвоена каждой птице) """
+        '''функция, которая задает значение логической переменной loaded (присвоена каждой птице) '''
         self.loaded = False
 
     def project_path(self):
-        
+        '''функция прорисовывает прицел'''
         if self.loaded:
             path = []
             ball = Pig(self.x, self.y, self.r, self.velocity, self.type)
-            for i in range(50):#рэнг отвечает за прицел!!!
+            for i in range(50):
                 ball.move()
                 if i%5 == 0:
                     path.append((ball.x, ball.y))
 
             for point in path:
-                pygame.draw.ellipse(display, self.color, (point[0], point[1], 2, 2)) #прорисовка прицела
+                pygame.draw.ellipse(display, self.color, (point[0], point[1], 2, 2)) 
 
 
 class Block:
+    '''класс ящик'''
     def __init__(self, x, y, r, v=None, color=( 120, 40, 31 ), colorBoundary = ( 28, 40, 51 )):
+        '''координаты и параметры ящиков'''
         self.r = 50 
         self.w = 100 
         self.h = 100
@@ -188,45 +194,27 @@ class Block:
         self.image = self.block_image
 
         if v == None:
-            self.velocity = Vector() #присваивает скорости значение нулевого вектора
+            self.velocity = Vector() 
         else:
             self.velocity = v
 
-        self.color = color
-        self.colorDestroyed = ( 100, 30, 22 )
-        self.colorBoundary = colorBoundary
-        self.rotateAngle = radians(0)
-        self.anchor = (self.r, self.r) 
-
-        self.isDestroyed = False #когда инициализируем блок - не разрушена
-
-    def rotate(self, coord, angle, anchor=(0, 0)):
-        """ вращение, возвращает два каких-то значения
-
-        Arguments:
-        coord - массив с координатами ЧЕГО ТОЛЬКО
-        angle -
-        anchor - привязка ()
-        """
-        corr = 0
-        return ((coord[0] - anchor[0])*cos(angle + radians(corr)) - (coord[1] - anchor[1])*sin(angle + radians(corr)),
-                (coord[0] - anchor[0])*sin(angle + radians(corr)) + (coord[1] - anchor[1])*cos(angle + radians(corr)))
+        self.isDestroyed = False 
 
     def translate(self, coord):
-        """возвращает значние каких-то других координат"""
+        '''возвращает значние каких-то других координат'''
         return [coord[0] + self.x, coord[1] + self.y]
 
     def draw(self):
-        pygame.transform.rotate(self.image, self.rotateAngle) #поворачивает изображение блока на угол 
-        display.blit(self.image, (self.x - self.w/2, self.y))###
+        '''прорисовка изображения'''
+        display.blit(self.image, (self.x - self.w/2, self.y))
 
     def destroy(self):
-        """ функция говорит что ящик сломан """
+        '''функция показывает, что ящик сломан'''
         self.isDestroyed = True
         self.image = self.block_destroyed_image
 
     def move(self):
-        """ движение блока """
+        '''движение ящика'''
         self.velocity = add_vectors(self.velocity, gravity)
 
         self.x += self.velocity.magnitude*sin(self.velocity.angle)
@@ -237,7 +225,7 @@ class Block:
         if self.x > width - self.w:
             self.x = 2*(width - self.w) - self.x
             self.velocity.angle *= -1
-            self.rotateAngle = - self.velocity.angle #блок как будто бы должен поворачиваться 
+            self.rotateAngle = - self.velocity.angle 
             self.velocity.magnitude *= block_elasticity
         elif self.x < self.w:
             self.x = 2*self.w - self.x
@@ -258,53 +246,37 @@ class Block:
 
 
 class Slingshot:
-    ''' Класс Рогатка
-
+    ''' Класс Рогатка '''
+    def __init__(self, x, y, w, h, color = (66, 73, 73)):
+        '''
         Arguments:
         x, y - координаты рогатки
-        w, h - ширина и высота рогатки соответственно
-        
-    '''
-    def __init__(self, x, y, w, h, color = (66, 73, 73)):
+        w, h - ширина и высота рогатки соответственно'''
         self.x = x
         self.y = y
         self.w = w
         self.h = h
         self.color = color
 
-    def rotate(self, coord, angle, anchor=(0, 0)):
-        """ снова функция поворота которая возвращает два каких-то непонятных значения """
-        corr = 0
-        return ((coord[0] - anchor[0])*cos(angle + radians(corr)) - (coord[1] - anchor[1])*sin(angle + radians(corr)), #anchor - это как бы массив со значениями
-                (coord[0] - anchor[0])*sin(angle + radians(corr)) + (coord[1] - anchor[1])*cos(angle + radians(corr)))
-
-    #def translate(self, coord):" ПОВОРОТ НИКОМУ НЕ НУЖНЫЙ
-        #""" возвращает какие-то значения """
-        #return [coord[0] + self.x, coord[1] + self.y]"
-
     def draw(self, loaded=None):
-        """ Функция прорисовывает рогатку во время прицеливания """
+        ''' Функция прорисовывает рогатку во время прицеливания '''
         pygame.draw.rect(display, self.color, (self.x, self.y + self.h*1/3, self.w, self.h*2/3))
         
         if (not loaded == None) and loaded.loaded: 
             pygame.draw.line(display, ( 100, 30, 22 ), (self.x - self.w/4 + self.w/4, self.y + self.h/6), (loaded.x, loaded.y + loaded.r/2), 10)
             pygame.draw.line(display, ( 100, 30, 22 ), (self.x + self.w, self.y + self.h/6), (loaded.x + loaded.r, loaded.y + loaded.r/2), 10)
-        #прорисовка рогатки
+        
         pygame.draw.rect(display, self.color, (self.x - self.w/4, self.y, self.w/2, self.h/3), 5)
         pygame.draw.rect(display, self.color, (self.x + self.w - self.w/4, self.y, self.w/2, self.h/3), 5)
 
 def collision_handler(b_1, b_2, type):
     '''
         функция проверяет столкновения
-
-        Arguments:
-        b_1, b_2 - объекты, между которыми происходит столкновение
-        type - тип объекта (какого??)
-        ПЕРЕДЕЛАТЬ СТОЛКНОВЕНИЯ!!!
+        b_1, b_2 - объекты, между которыми происходит столкновение (блок - шар) или (шар - шар)
     '''
     collision = False
     if type == "BALL":
-        dx = b_1.x - b_2.x
+        dx = b_1.x - b_2.x + 4
         dy = b_1.y - b_2.y
 
         dist = hypot(dx, dy)
@@ -330,23 +302,21 @@ def collision_handler(b_1, b_2, type):
             b_2.x -= sin(angle)*overlap
             b_2.y += cos(angle)*overlap
             collision = True
-            #print(collision)
-
-        #print(collision)
+        
         return b_1, b_2, collision
     elif type == "BALL_N_BLOCK":
         dx = b_1.x - b_2.x
         dy = b_1.y - b_2.y
 
         dist = hypot(dx, dy)
-        if abs(b_1.x - b_2.x) < b_2.w/2 + b_1.r and abs(b_1.y - b_2.y) < b_2.h/2 + b_1.r :#dist < b_1.r + b_2.w/2:
+        if abs(b_1.x - b_2.x) < b_2.w/2 + b_1.r and abs(b_1.y - b_2.y) < b_2.h/4 + b_1.r :
             tangent = atan2(dy, dx)
             angle = 0.5 * pi + tangent
 
             angle1 = b_1.velocity.angle + pi
             angle2 = b_1.velocity.angle
 
-            magnitude1 = b_1.velocity.magnitude / 2
+            magnitude1 = b_1.velocity.magnitude / 10
             magnitude2 = b_1.velocity.magnitude / 10
 
             b_1.velocity = Vector(magnitude1, angle1)
@@ -355,30 +325,14 @@ def collision_handler(b_1, b_2, type):
             b_1.velocity.magnitude *= elasticity
             b_2.velocity.magnitude *= block_elasticity
 
-            ###overlap = (b_1.r - dist + sin(angle)*b_2.h / 2 - 1)####overlap = (b_1.r - dist + sin(angle)*b_2.h/2 - 1)
-            #overlap = abs(b_1.r - dist + sin(angle) * b_2.h/2 - 1)
             overlap = 0.5*(b_1.r + b_2.w - dist + 1)
-            b_1.x += sin(angle)*overlap / 10
-            b_1.y -= cos(angle)*overlap / 10 
-            b_2.x -= sin(angle)*overlap / 10
-            b_2.y += cos(angle)*overlap / 10
+            b_1.x += sin(angle)*overlap / 5
+            b_1.y -= cos(angle)*overlap / 5 
+            b_2.x -= sin(angle)*overlap / 5
+            b_2.y += cos(angle)*overlap / 5
             collision = True
-        #if b_1.y < b_2.y and b_2.y - b_1.y < b_1.r + b_1.h / 2
 
         return b_1, b_2, collision
-
-def smash (block, block2):
-    angle1 = block2.velocity.angle
-    angle2 = block.velocity.angle
-
-    magnitude1 = block2.velocity.magnitude
-    magnitude2 = block.velocity.magnitude
-
-    block.velocity = Vector(magnitude1, angle1)
-    block2.velocity = Vector(magnitude2, angle2)
-
-    block.velocity.magnitude *= elasticity / 13
-    block2.velocity.magnitude *= elasticity / 13
     
 
 def block_collision_handler(block, block2):
@@ -396,11 +350,9 @@ def block_collision_handler(block, block2):
         elif block.x + block.w/2 > block2.x and (block.x < block2.x):
             block.x = 2*(block2.x - block.w/2) - block.x
             block.velocity.angle = - block.velocity.angle
-            block.rotateAngle = - block.velocity.angle
             block.velocity.magnitude *= block_elasticity
 
             block2.velocity.angle = - block2.velocity.angle
-            block2.rotateAngle = - block2.velocity.angle
             block2.velocity.magnitude *= block_elasticity
             collision = True
 
@@ -408,22 +360,18 @@ def block_collision_handler(block, block2):
         if block.y + block.h > block2.y and block.y < block2.y:
             block.y = 2*(block2.y - block.h) - block.y
             block.velocity.angle = pi - block.velocity.angle
-            block.rotateAngle = pi - block.velocity.angle
             block.velocity.magnitude *= block_elasticity
 
             block2.velocity.angle = pi - block2.velocity.angle
-            block2.rotateAngle = pi - block2.velocity.angle
             block2.velocity.magnitude *= block_elasticity
             collision = True
 
         elif (block.y < block2.y + block2.h) and (block.y + block.h > block2.y + block2.h):
             block.y = 2*(block2.y + block2.h/2) - block.y
             block.velocity.angle = pi - block.velocity.angle
-            block.rotateAngle = pi - block.velocity.angle
             block.velocity.magnitude *= block_elasticity
 
             block2.velocity.angle = pi - block2.velocity.angle
-            block2.rotateAngle = pi - block2.velocity.angle
             block2.velocity.magnitude *= block_elasticity
             collision = True
 
